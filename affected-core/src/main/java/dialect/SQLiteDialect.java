8d1c73a1850e5565379d640545613a0373ba3651
Evelina Vrabie <evelyne24@gmail.com>
2010-01-08 23:46:47 +0000

    Copyright Notice

--- affected-core/src/main/java/dialect/SQLiteDialect.java
+++ affected-core/src/main/java/dialect/SQLiteDialect.java
@@ -1,13 +1,14 @@
-package dialect;
 /*
  * The author disclaims copyright to this source code. In place of
  * a legal notice, here is a blessing:
- * 
+ *
  * May you do good and not evil.
  * May you find forgiveness for yourself and forgive others.
  * May you share freely, never taking more than you give.
  *
  */
+
+package dialect;
 
 import org.hibernate.Hibernate;
 import org.hibernate.dialect.Dialect;
--- affected-core/src/main/java/org/codeandmagic/affected/svn/api/SvnException.java
+++ affected-core/src/main/java/org/codeandmagic/affected/svn/api/SvnException.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Copyright© 2010 Cristian Vrabie, Evelina Petronela Vrabie
+ *
+ * This file is part of Affected.
+ *
+ * Affected is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Affected is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Affected.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 package org.codeandmagic.affected.svn.api;
 
 /** General type of exception, mostly used as a wrapper for other custom exceptions. */
